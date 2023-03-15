50f1eded252d0f77f5e9304ec2dcecadae58e68c
gabbyca <gabrielle_cammarata@mynbps.org>
2023-03-15 12:07:49 -0400

    COMMIT

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,7 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.util.Units; 
 
 public final class Constants {
   public static final class DriveConstants {
