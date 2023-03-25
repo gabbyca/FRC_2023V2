830961a2f7c3c95151c771ad7ac419051171fdaf
gabbyca <gabrielle_cammarata@mynbps.org>
2023-03-25 07:27:03 -0400

    auto

--- src/main/java/frc/robot/commands/autoCommandHolder/Intake.java
+++ src/main/java/frc/robot/commands/autoCommandHolder/Intake.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.AutoCommandHolder; 
+package frc.robot.commands.AutoCommandHolder;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
