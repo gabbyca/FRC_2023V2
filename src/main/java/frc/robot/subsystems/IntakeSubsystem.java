458cd294b3d901ae59ca71bbed4f15bf74e9ec16
gabbyca <gabrielle_cammarata@mynbps.org>
2023-04-06 00:01:56 -0500

    tym current code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,63 +2,34 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.IntakeConstants.*;
+
 
 public class IntakeSubsystem extends SubsystemBase {
     
-     
   private final CANSparkMax m_intake;
   public boolean inverted = false;
   public double speed = 0;
-
-  
-  private static final double CURRENT_SPIKE_THRESHOLD = 38.0; // expected spike 
-  private static final int DEBOUNCE_DELAY = 25; // required debounce time (ms)
-  private long lastSpikeTime;
-
+  //double current = 0;
+  public static double filtered_current = 0; // always keep track of filtered current
+  private LinearFilter filter = LinearFilter.movingAverage(SAMPLE_SIZE);
   public IntakeSubsystem(){
     m_intake = new CANSparkMax(13, MotorType.kBrushless);
-    lastSpikeTime = System.currentTimeMillis();
-  }
-  
-  public void intake(double speed, boolean inverted){
-    this.speed = speed; 
-    this.inverted = inverted; 
-      if (isDebounced() && checkCurrentSpike()) 
-        stopIntake();
-      else {
-        m_intake.set(speed);
-        m_intake.setInverted(inverted);
-      }
   }
 
-  public void releaseIntake(double speed, boolean inverted){
+  public void setIntakeSpeedDirection(double speed, boolean inverted){
     this.speed = speed; 
-    this.inverted = inverted; 
+    this.inverted = inverted;
     m_intake.set(speed);
     m_intake.setInverted(inverted);
   }
 
-  public void stopIntake() {
-      m_intake.set(0);
-  }
-
-  private boolean checkCurrentSpike() {
-    double current = m_intake.getOutputCurrent();
-    System.out.println(current);
-    return current >= CURRENT_SPIKE_THRESHOLD;
-  }
-
-  private boolean isDebounced() {
-    long currentTime = System.currentTimeMillis();
-    if (currentTime - lastSpikeTime >= DEBOUNCE_DELAY) {
-        lastSpikeTime = currentTime;
-        return true;
-    }
-    return false;
-  }
-
   @Override
-  public void periodic(){}
-
+  public void periodic(){
+    filtered_current = filter.calculate(m_intake.getOutputCurrent());
+    System.out.println("Filtered Current: " + filtered_current);
+  }
 }
