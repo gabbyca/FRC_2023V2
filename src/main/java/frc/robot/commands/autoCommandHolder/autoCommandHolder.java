3704a0d6d987464cab0bbd6c25d9d70976af470c
AnthonyNBP <anthonylweiss@icloud.com>
2023-04-06 17:46:43 -0500

    commit

--- src/main/java/frc/robot/commands/autoCommandHolder/autoCommandHolder.java
+++ src/main/java/frc/robot/commands/autoCommandHolder/autoCommandHolder.java
@@ -8,9 +8,6 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
-
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.IntakeSubsystem;
